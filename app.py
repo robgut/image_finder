c70c6b6bbd4df35df97dbcf155410d1554ce1513
RG <limesrg@hotmail.com>
2025-01-28 08:18:25 +0100

    add cwd

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-
+import os
 
 def save_photo(upl_file):
     try:
         buff = upl_file.getbuffer()
-        with open(upl_file.name, 'wb') as f:
+        with open('01_' + upl_file.name, 'wb') as f:
             f.write(buff)
         return True
     except Exception as ex:
@@ -12,7 +12,7 @@ def save_photo(upl_file):
 
 
 st.title('Aplikacja do przesyłania zdjęć')
-
+st.write(f'cwd:{os.getcwd()}')
 uploaded_file = st.file_uploader('Wybierz zdjęcie', type=['png', 'jpeg', 'jpg'], accept_multiple_files=False)
 
 if uploaded_file is not None:
@@ -20,6 +20,3 @@ if uploaded_file is not None:
         st.success("Plik został pomyślnie zapisany!")
     else:
         st.error('Nie udało się zapisać pliku...')
-
-
-
